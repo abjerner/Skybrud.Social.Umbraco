bef5e682353506aaf310fd06eb0ee0a6b954393e
Anders Bjerner <abjerner@skybrud.dk>
2016-02-13 23:50:43 +0100

    Facebook: The confirm button will now shows the amount of selected scopes

--- src/Skybrud.Social.Umbraco/App_Plugins/Skybrud.Social/Facebook/OAuth/Dialogs.js
+++ src/Skybrud.Social.Umbraco/App_Plugins/Skybrud.Social/Facebook/OAuth/Dialogs.js
@@ -2,14 +2,17 @@
 
     var selection = Array.isArray($scope.dialogOptions.selection) ? $scope.dialogOptions.selection : [];
 
-    $http.get('/umbraco/SkybrudSocial/Facebook/GetScopes').success(function (r) {
-        $scope.scopes = r;
-        angular.forEach($scope.scopes, function (group) {
-            angular.forEach(group.scopes, function (scope) {
-                scope.checked = selection.indexOf(scope.Name) >= 0;
+    $scope.count = 0;
+
+    $scope.update = function() {
+        var count = 0;
+        angular.forEach($scope.scopes, function(group) {
+            angular.forEach(group.scopes, function(scope) {
+                if (scope.checked) count++;
             });
         });
-    });
+        $scope.count = count;
+    };
 
     $scope.confirm = function() {
         var temp = [];
@@ -22,5 +25,19 @@
         });
         $scope.submit(temp);
     };
+
+    $http.get('/umbraco/SkybrudSocial/Facebook/GetScopes').success(function (r) {
+        $scope.scopes = r;
+        var count = 0;
+        angular.forEach($scope.scopes, function (group) {
+            angular.forEach(group.scopes, function (scope) {
+                scope.checked = selection.indexOf(scope.Name) >= 0;
+                if (scope.checked) {
+                    count++;
+                }
+            });
+        });
+        $scope.count = count;
+    });
 
 });
