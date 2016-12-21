6530d95c286c758fce36bf90c5724016c7617347
Anders Bjerner <abjerner@skybrud.dk>
2016-12-21 20:07:46 +0100

    Did a little rework on the API for Google scopes so we can have more information about each scope

--- src/Skybrud.Social.Umbraco/Controllers/Api/GoogleController.cs
+++ src/Skybrud.Social.Umbraco/Controllers/Api/GoogleController.cs
@@ -1,7 +1,4 @@
 ﻿using Newtonsoft.Json;
-using Skybrud.Social.Google.Analytics;
-using Skybrud.Social.Google.OAuth;
-using Skybrud.Social.Google.YouTube;
 using Skybrud.Social.Umbraco.Google.Scope;
 using Skybrud.Social.Umbraco.WebApi;
 using Umbraco.Web.Mvc;
@@ -25,30 +22,50 @@ namespace Skybrud.Social.Umbraco.Controllers.Api
     }
 }
 
-namespace Skybrud.Social.Umbraco.Google.Scope
-{
-    internal static class GoogleScopeGroups
-    {
+namespace Skybrud.Social.Umbraco.Google.Scope {
+    
+    internal static class GoogleScopeGroups {
+        
         public static readonly GoogleScopeGroup Basic = new GoogleScopeGroup("Basic", new[] {
-            GoogleScopes.Email,
-            GoogleScopes.OpenId,
-            GoogleScopes.Profile
+            new GoogleScopeItem { Alias = "openid", Type = "global", Prefix = "", Name = "OpenID", Description = "" },
+            new GoogleScopeItem { Alias = "email", Type = "global", Prefix = "", Name = "Email", Description = "" },
+            new GoogleScopeItem { Alias = "profile", Type = "global", Prefix = "", Name = "Profile", Description = "" }
         });
 
         public static readonly GoogleScopeGroup Analytics = new GoogleScopeGroup("Analytics", new[] {
-            AnalyticsScopes.Readonly,
-            AnalyticsScopes.Write,
-            AnalyticsScopes.Edit,
-            AnalyticsScopes.ManageUsers,
-            AnalyticsScopes.ManageUsersReadonly,
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/analytics.readonly", Type = "analytics", Prefix = "Analytics: ", Name = "Read-only access", Description = "Read-only access to the Analytics API." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/analytics", Type = "analytics", Prefix = "Analytics: ", Name = "Write access", Description = "Write access to the Analytics API." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/analytics.edit", Type = "analytics", Prefix = "Analytics: ", Name = "Edit", Description = "Edit Google Analytics management entities." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/analytics.manage.users", Type = "analytics", Prefix = "Analytics: ", Name = "Manage users", Description = "	View and manage user permissions for Analytics accounts." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/analytics.manage.users.readonly", Type = "analytics", Prefix = "Analytics: ", Name = "Manage users (readonly)", Description = "View Google Analytics user permissions." }
         });
 
         public static readonly GoogleScopeGroup YouTube = new GoogleScopeGroup("YouTube", new[] {
-            YouTubeScopes.Readonly,
-            YouTubeScopes.Manage,
-            YouTubeScopes.Upload,
-            YouTubeScopes.PartnerChannelAudit
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/youtube", Type = "youtube", Prefix = "YouTube: ", Name = "Manage", Description = "Manage your YouTube account." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/youtube.readonly", Type = "youtube", Prefix = "YouTube: ", Name = "Readonly", Description = "View your YouTube account." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/youtube.upload", Type = "youtube", Prefix = "YouTube: ", Name = "Upload", Description = "Upload YouTube videos and manage your YouTube videos." },
+            new GoogleScopeItem { Alias = "https://www.googleapis.com/auth/youtubepartner-channel-audit", Type = "youtube", Prefix = "YouTube: ", Name = "Channel audit", Description = "Retrieve the auditDetails part in a channel resource." }
         });
+    
+    }
+
+    internal class GoogleScopeItem {
+
+        [JsonProperty("alias")]
+        public string Alias { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("prefix")]
+        public string Prefix { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
     }
 
     internal class GoogleScopeGroup
@@ -57,12 +74,12 @@ namespace Skybrud.Social.Umbraco.Google.Scope
         public string Name { get; private set; }
 
         [JsonProperty("scopes")]
-        public GoogleScope[] Scopes { get; private set; }
+        public GoogleScopeItem[] Scopes { get; private set; }
 
-        public GoogleScopeGroup(string name, GoogleScope[] scopes)
+        public GoogleScopeGroup(string name, GoogleScopeItem[] scopes)
         {
             Name = name;
-            Scopes = scopes ?? new GoogleScope[0];
+            Scopes = scopes ?? new GoogleScopeItem[0];
         }
     }
 }
