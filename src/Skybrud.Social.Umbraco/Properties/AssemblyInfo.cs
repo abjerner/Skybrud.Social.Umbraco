3d2903f0eb1c2d4dba9fe8c4b1cbcf87d932855e
Anders Bjerner <abjerner@skybrud.dk>
2017-01-11 23:08:45 +0100

    New release :D

--- src/Skybrud.Social.Umbraco/Properties/AssemblyInfo.cs
+++ src/Skybrud.Social.Umbraco/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skybrud.dk")]
 [assembly: AssemblyProduct("Skybrud.Social.Umbraco")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyInformationalVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyInformationalVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
